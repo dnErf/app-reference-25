3f7557fa783297e46b3903a814fda2df8d183e5e
ff <frend.fernando@LIVE.COM>
2024-12-11 16:57:54 -0800

    up e2ee

--- e2ee-client/astro.config.mjs
+++ e2ee-client/astro.config.mjs
@@ -5,11 +5,20 @@ import tailwind from '@astrojs/tailwind';
 
 import node from '@astrojs/node';
 
+import { loadEnv } from 'vite'
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '')
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  vite: {
+    define: {
+      __APP_ENV__: JSON.stringify(env)
+    }
+  }
 });
