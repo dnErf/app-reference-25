0d769e6ede3b5950b055369eb9da08b13294be87
ff <frend.fernando@LIVE.COM>
2024-12-17 19:01:19 -0800

    up e2ee

--- e2ee-internal/main.py
+++ e2ee-internal/main.py
@@ -1,1 +1,25 @@
-print("hello")
+import asyncio
+import redis.asyncio as redis
+
+async def pubsub_reader(chn: redis.client.PubSub):
+    while True:
+        msg = await chn.get_message(ignore_subscribe_messages=True, timeout=None)
+
+        if msg is None: 
+            continue
+
+        print(f'{type(msg['data'])}')
+        if isinstance(msg['data'], bytes) and msg['data'].decode() == 'STOP':
+            break
+
+        print(f'message received: {msg}')
+
+async def main():
+    red = redis.from_url('redis://172.30.55.194:6379')
+    async with red.pubsub() as pubsub:
+        await pubsub.subscribe('zchn_test')
+        future = asyncio.create_task(pubsub_reader(pubsub))
+        await future
+
+if __name__ == "__main__":
+    asyncio.run(main())
